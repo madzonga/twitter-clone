0612b741c6e4aded83d62feba9d4a3f2836e4127
Glenn Madzonga <glenn.madzonga@gmail.com>
2024-07-19 22:56:49 +0200

    extract and handle mentions

--- src/controllers/tweetController.ts
+++ src/controllers/tweetController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import Tweet from '../models/Tweet';
 import User from '../models/User';
+import Tag from '../models/Tag';
 
 const createTweet = async (req: Request, res: Response) => {
   const errors = validationResult(req);
@@ -19,6 +20,21 @@ const createTweet = async (req: Request, res: Response) => {
       userId: user.id,
     });
 
+    // Extract and handle mentions
+    const mentions = content.match(/@\w+/g);
+    if (mentions) {
+      for (const mention of mentions) {
+        const username = mention.slice(1);
+        const taggedUser = await User.findOne({ where: { username } });
+        if (taggedUser) {
+          await Tag.create({
+            tweetId: tweet.id,
+            userId: taggedUser.id,
+          });
+        }
+      }
+    }
+
     res.status(201).json(tweet);
   } catch (err: any) {
     console.error(err.message);
@@ -26,4 +42,27 @@ const createTweet = async (req: Request, res: Response) => {
   }
 };
 
-export { createTweet };
+const getTweetsByMention = async (req: Request, res: Response) => {
+  try {
+    const user = req.user as User;
+    const tweets = await Tweet.findAll({
+      include: [
+        {
+          model: User,
+          through: {
+            attributes: [],
+          },
+          where: {
+            id: user.id,
+          },
+        },
+      ],
+    });
+    res.json(tweets);
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+export { createTweet, getTweetsByMention };
